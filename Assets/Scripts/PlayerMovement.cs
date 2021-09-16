bf6d4c168e66a0d3a0127a4a917fba3a65e92da6
Rick Davidson <rickinaction@gmail.com>
2021-09-16 17:56:38 +1000

    29 Instantiate Bullet From Gun

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
     [SerializeField] Vector2 deathKick = new Vector2 (10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
     
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
@@ -37,6 +39,12 @@ public class PlayerMovement : MonoBehaviour
         Die();
     }
 
+    void OnFire(InputValue value)
+    {
+        if (!isAlive) { return; }
+        Instantiate(bullet, gun.position, transform.rotation);
+    }
+    
     void OnMove(InputValue value)
     {
         if (!isAlive) { return; }
