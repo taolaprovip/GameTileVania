c077df4aa71c57736c0fa1a13ec9a335a24d20e7
Rick Davidson <rickinaction@gmail.com>
2021-09-24 10:16:54 +1000

    36 Persistent Score & Lives

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
     
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Player")
         {
+            FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
