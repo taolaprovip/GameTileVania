02c98ad200c6c40d4d62e72b17590f9a8572c082
Rick Davidson <rickinaction@gmail.com>
2021-09-25 11:19:13 +1000

    37 Scene Persist

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -60,6 +60,7 @@ public class GameSession : MonoBehaviour
 
     void ResetGameSession()
     {
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
         SceneManager.LoadScene(0);
         Destroy(gameObject);
     }
